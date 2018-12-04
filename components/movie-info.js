35d84c78934e3868b8d5f4e14a8b66b288a98612
Aurelio Pita <jaapaurelio@gmail.com>
2018-12-04 01:01:11 +0000

    Get list of movies

--- components/movie-info.js
+++ components/movie-info.js
@@ -1,219 +1,172 @@
-
-export default ({movie}) => (
-    <section>
-        props: {JSON.stringify(movie)}
-        <div className="movie-backdrop">
-            <img src="https://image.tmdb.org/t/p/w700_and_h392_bestv2/bHarw8xrmQeqf3t8HpuMY7zoK4x.jpg" />
+export default ({ movie }) => (
+  <section>
+    <div className="movie-backdrop">
+      <img
+        src={
+          "https://image.tmdb.org/t/p/w700_and_h392_bestv2" +
+          movie.backdrop_path
+        }
+      />
+    </div>
+    <div className="main-info">
+      <img
+        className="movie-poster"
+        src={
+          "https://image.tmdb.org/t/p/w174_and_h261_bestv2" + movie.poster_path
+        }
+      />
+      <div className="info-row">
+        <span className="movie-title">{movie.title}</span>
+        &nbsp;
+        <span className="movie-year">({movie.release_date.split("-")[0]})</span>
+        <div>
+          <span className="gender">{movie.genres.join(", ")}</span>
         </div>
-        <div className="main-info">
-            <img className="movie-poster" src="https://image.tmdb.org/t/p/w174_and_h261_bestv2/y31QB9kn3XSudA15tV7UWQ9XLuW.jpg" />
-            <div className="info-row">
-                <span className="movie-title">
-                    {movie.title}
-                </span>
-                &nbsp;
-                <span className="movie-year">
-                    (2018)
-                </span>
-                <div>
-                    <span className="gender">
-                        Action, Science Fiction, Adventure
-                    </span>
-                </div>
-                <span className="time">2h 1m &nbsp;</span>
-                <span className="more-18">+18</span>
+        <span className="time">2h 1m &nbsp;</span>
+        {movie.adult && <span className="more-18">+18</span>}
+      </div>
+    </div>
+    <div className="content-padding movie-description">
+      <h3>Overview</h3>
+      {movie.overview}
+      <div>
+        <br />
+        <strong>James Gunn</strong>
+        <br />
+        Director
+      </div>
+    </div>
+    <div className="content-padding actors-container">
+      <h3>Cast</h3>
 
-            </div>
+      <div className="actors">
+        <div className="actor-container">
+          <img
+            className="actor-image"
+            src="https://image.tmdb.org/t/p/w240_and_h266_face/gXKyT1YU5RWWPaE1je3ht58eUZr.jpg"
+          />
+          <div className="actor-name">Chris Pratt</div>
+          <div className="actor-persona">Peter Quill / Star-Lord</div>
         </div>
-
-        <div className="content-padding movie-description">
-            <h3>Overview</h3>
-            {movie.overview}
-            <div>
-                <br/>
-                <strong>James Gunn</strong>
-                <br/>Director
-            </div>
+        <div className="actor-container">
+          <img
+            className="actor-image"
+            src="https://image.tmdb.org/t/p/w240_and_h266_face/ofNrWiA2KDdqiNxFTLp51HcXUlp.jpg"
+          />
+          <div className="actor-name">Zoe Saldana</div>
+          <div className="actor-persona">Gamora</div>
         </div>
+      </div>
+    </div>
 
-        <div className="content-padding actors-container">
-            <h3>Cast</h3>
+    <style jsx>
+      {`
+        .content-padding {
+          padding: 0px 20px;
+        }
 
-            <div className="actors">
-                <div className="actor-container">
-                    <img className="actor-image" src="https://image.tmdb.org/t/p/w240_and_h266_face/gXKyT1YU5RWWPaE1je3ht58eUZr.jpg" />
-                    <div className="actor-name">Chris Pratt</div>
-                    <div className="actor-persona">Peter Quill / Star-Lord</div>
-                </div>
-                <div className="actor-container">
-                    <img className="actor-image" src="https://image.tmdb.org/t/p/w240_and_h266_face/ofNrWiA2KDdqiNxFTLp51HcXUlp.jpg" />
-                    <div className="actor-name">Zoe Saldana</div>
-                    <div className="actor-persona">Gamora</div>
-                </div>
-            </div>
-        </div>
+        .movie-backdrop {
+          text-align: center;
+        }
 
-        <div className="buttons-container-space"></div>
-        <div className="buttons-container">
-            <div>
-                <div className="button-choice button-no">
-                    Not today
-                </div>
-                <div className="button-choice button-yes">
-                    Yes please
-                </div>
-            </div>
-        </div>
+        .movie-backdrop img {
+          max-width: 100%;
+        }
 
-        <style jsx>{`
-            .buttons-container {
-                position: fixed;
-                bottom: 0;
-                background: #f9fcff;
-                left: 0;
-                right: 0;
-                border-top: 1px solid #e8ebef;
-                text-align: center;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
+        .movie-poster {
+          width: 100px;
+          position: absolute;
+          top: -50px;
+          left: 10px;
+        }
 
-            .buttons-container-space {
-                margin-top: 100px;
-            }
+        .main-info {
+          padding: 20px 0;
+          position: relative;
+          background: #1f1f1f;
+          color: #fff;
+          margin-top: -10px;
+        }
 
-            .button-no {
-                background: #ff565e;
-                cursor: pointer;
-            }
+        .info-row {
+          margin-left: 120px;
+          margin-bottom: 6px;
+        }
 
-            .button-yes {
-                background: #00e390;
-                cursor: pointer;
-            }
+        .big-space {
+          margin-bottom: 14px;
+        }
 
-            .button-choice {
-                padding: 6px 20px;
-                display: inline-block;
-                margin: 20px;
-                border-radius: 4px;
-                color: #FFF;
-                text-transform: uppercase;
-                font-size: 12px;
+        .movie-title {
+          font-weight: bold;
+        }
 
-            }
-        `}
-        </style>
+        .gender {
+          display: inline-block;
+          margin-right: 4px;
+          padding: 2px;
+          font-size: 12px;
+        }
 
-        <style jsx>{`
-            .content-padding {
-                padding: 0px 20px;
-            }
+        .time {
+          font-size: 12px;
+        }
 
-            .movie-backdrop {
-                text-align: center;
-            }
+        .more-18 {
+          border: 1px solid red;
+          font-size: 12px;
+          padding: 0 2px;
+          color: red;
+        }
 
-            .movie-backdrop img {
-                max-width: 100%;
-            }
+        .movie-description {
+          padding-top: 20px;
+          padding-bottom: 20px;
+          font-size: 12px;
+          background: #143846;
+          color: #fff;
+          line-height: 1.5;
+        }
+      `}
+    </style>
+    <style jsx>
+      {`
+        h3 {
+          font-size: 15px;
+          font-weight: bold;
+          margin-bottom: 10px;
+        }
+        strong {
+          font-weight: bold;
+        }
 
-            .movie-poster {
-                width: 100px;
-                position: absolute;
-                top: -50px;
-                left: 10px;
-            }
+        .actors-container {
+          margin: 20px 0;
+        }
 
-            .main-info {
-                padding: 20px 0;
-                position: relative;
-                background: #1f1f1f;
-                color: #fff;
-                margin-top: -10px;
-            }
+        .actors {
+          display: flex;
+        }
 
-            .info-row {
-                margin-left: 120px;
-                margin-bottom: 6px;
-            }
+        .actor-container {
+          margin-top: 10px;
+          width: 100px;
+          margin-right: 10px;
+        }
 
-            .big-space {
-                margin-bottom: 14px;
+        .actor-image {
+          max-width: 100%;
+        }
 
-            }
+        .actor-name {
+          font-size: 12px;
+          font-weight: bold;
+        }
 
-            .movie-title {
-                font-weight: bold;
-            }
-
-            .gender {
-                display: inline-block;
-                margin-right: 4px;
-                padding: 2px;
-                font-size: 12px;
-            }
-
-            .time {
-                font-size: 12px;
-            }
-
-            .more-18 {
-                border: 1px solid red;
-                font-size: 12px;
-                padding: 0 2px;
-                color: red;
-            }
-
-            .movie-description {
-                padding-top: 20px;
-                padding-bottom: 20px;
-                font-size: 12px;
-                background: #143846;
-                color: #fff;
-                line-height: 1.5;
-            }
-        `}
-        </style>
-
-        <style jsx>{`
-            h3 {
-                font-size: 15px;
-                font-weight: bold;
-                margin-bottom: 10px;
-            }
-            strong {
-                font-weight: bold;
-            }
-
-            .actors-container {
-                margin: 20px 0;
-            }
-
-            .actors {
-                display: flex;
-            }
-
-            .actor-container {
-                margin-top: 10px;
-                width: 100px;
-                margin-right: 10px;
-            }
-
-            .actor-image {
-                max-width: 100%;
-            }
-
-            .actor-name {
-                font-size: 12px;
-                font-weight: bold;
-            }
-
-            .actor-persona {
-                font-size: 12px;
-            }
-        `}
-        </style>
-    </section>
+        .actor-persona {
+          font-size: 12px;
+        }
+      `}
+    </style>
+  </section>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,126 @@
-import Topbar from '../components/topbar'
-import MovieInfo from '../components/movie-info';
-import SwipeArea from '../components/swipe-area';
-const MovieDb = require('moviedb-promise')
-const moviedb = new MovieDb('284941729ae99106f71e56126227659b')
+import Topbar from "../components/topbar";
+import MovieInfo from "../components/movie-info";
+import SwipeArea from "../components/swipe-area";
+const MovieDb = require("moviedb-promise");
+const moviedb = new MovieDb("284941729ae99106f71e56126227659b");
 
-const Index = (props) => (
-    <div>
-        <Topbar title="Movie Match" />
-        <SwipeArea>
-            {console.log(props.movies)}
-            ---
-            <MovieInfo movie={props.movies.results[4]} />
-        </SwipeArea>
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.like = this.like.bind(this);
+    this.noLike = this.noLike.bind(this);
 
-        <style jsx>{`
-        p {
-            color: red;
-        }
-        `}
-        </style>
-
-    </div>
-  )
-
-  Index.getInitialProps = function() {
-    return moviedb.miscTopRatedMovies()
-        .then(movies => {
-            console.log(movies);
-            movies.results.map(movie => moviedb.movieInfo({ id: movie.id }));
-        }
-    );
-
+    this.state = {
+      id: 2
+    };
   }
 
-  export default Index
+  getNewMovie() {
+    this.setState({
+      id: Math.floor(Math.random() * 20 + 1)
+    });
+  }
+
+  like() {
+    this.getNewMovie();
+  }
+
+  noLike() {
+    this.getNewMovie();
+  }
+
+  render() {
+    const { movies, id } = this.props;
+
+    return (
+      <div>
+        <Topbar title="Movie Match" />
+        <SwipeArea>
+          <MovieInfo movie={movies[this.state.id]} />
+        </SwipeArea>
+
+        <div className="buttons-container-space" />
+        <div className="buttons-container">
+          <div>
+            <div onClick={this.noLike} className="button-choice button-no">
+              Not today
+            </div>
+            <div onClick={this.like} className="button-choice button-yes">
+              Yes please
+            </div>
+          </div>
+        </div>
+
+        <style jsx>
+          {`
+            .buttons-container {
+              position: fixed;
+              bottom: 0;
+              background: #f9fcff;
+              left: 0;
+              right: 0;
+              border-top: 1px solid #e8ebef;
+              text-align: center;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+
+            .buttons-container-space {
+              margin-top: 100px;
+            }
+
+            .button-no {
+              background: #ff565e;
+              cursor: pointer;
+            }
+
+            .button-yes {
+              background: #00e390;
+              cursor: pointer;
+            }
+
+            .button-choice {
+              padding: 6px 20px;
+              display: inline-block;
+              margin: 20px;
+              border-radius: 4px;
+              color: #fff;
+              text-transform: uppercase;
+              font-size: 12px;
+            }
+          `}
+        </style>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.setState({
+      id: Math.floor(Math.random() * 20 + 1)
+    });
+  }
+
+  static async getInitialProps() {
+    const genreList = await moviedb.genreMovieList();
+    const genres = genreList.genres.reduce((acc, genre) => {
+      acc[genre.id] = genre;
+      return acc;
+    }, {});
+
+    const movieList = await moviedb.miscTopRatedMovies({
+      append_to_response: "videos"
+    });
+
+    const movies = movieList.results.map(movie => ({
+      ...movie,
+      genres: movie.genre_ids.map(genreId => genres[genreId].name)
+    }));
+
+    return {
+      movies
+    };
+  }
+}
+
+export default Index;
