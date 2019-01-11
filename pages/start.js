c493fac5a204105532ca0ae75d1b53ee8e3c0650
Aurelio Pita <jaapaurelio@gmail.com>
2019-01-11 00:03:26 +0000

    Improve start page

--- pages/start.js
+++ pages/start.js
@@ -2,6 +2,11 @@ import Topbar from "../components/topbar";
 import Link from "next/link";
 import Router from "next/router";
 
+const steps = [
+  "Create or join a room.",
+  "Say yes to movies you want to watch.",
+  "Discover the perfect movie to watch together."
+];
 class Start extends React.Component {
   constructor(props) {
     super(props);
@@ -30,31 +35,112 @@ class Start extends React.Component {
     return (
       <div>
         <Topbar activetab="room" title="Movie Match" />
-        <br />
-        <br />
-        <Link href={`/create-room`}>
-          <a>Create Room</a>
-        </Link>
-        <div className="info">
-          Create a room so friends can join you to find the perfect movie to
-          watch.
+        <div className="steps-area">
+          {steps.map((step, i) => {
+            return <div className="step-details">{step}</div>;
+          })}
         </div>
-        <br />
-        <br />
-        <div>Or join a room:</div>
-        Room number: <input type="text" onChange={this.onChangeId} />{" "}
-        <button onClick={this.join}>Join</button>
-        <div className="info">
-          Ask you friend for the room number and join him/her.
+        <div className="options-container">
+          <div className="create-room-btn-container">
+            <Link href={`/create-room`}>
+              <a className="start-btn">Create a Room</a>
+            </Link>
+          </div>
+
+          <div className="info">Friends will join you later.</div>
+        </div>
+        <div className="options-container">
+          <div className="join-title">Join a room</div>
+          <div>
+            <input
+              className="room-input"
+              type="text"
+              placeholder="xxxx"
+              maxlength="4"
+              onChange={this.onChangeId}
+            />
+            <button className="start-btn" onClick={this.join}>
+              Join
+            </button>
+          </div>
+
+          <div className="info">
+            Ask your friend for the room number and join him/her.
+          </div>
         </div>
         <style jsx>{`
-          input,
-          button {
-            border: 1px solid black;
-          }
-
           .info {
             font-size: 12px;
+            text-align: center;
+            margin-top: 10px;
+          }
+
+          .start-here {
+            font-size: 16px;
+            font-weight: bold;
+            margin: 12px 0;
+            padding: 0 20px;
+          }
+
+          .headline {
+            padding: 0 20px;
+            font-size: 14px;
+            margin-bottom: 10px;
+            text-align: center;
+          }
+
+          .steps-area {
+            padding: 20px;
+            font-family: "Thasadith", sans-serif;
+            background: aquamarine;
+            margin-bottom: 20px;
+          }
+
+          .steps-area h1 {
+            font-size: 16px;
+            font-weight: bold;
+            margin-bottom: 10px;
+          }
+
+          .step-details {
+            font-size: 16px;
+          }
+
+          .options-container {
+            padding: 20px;
+            text-align: center;
+          }
+
+          .create-room-btn-container {
+            padding: 0 10px;
+            text-align: center;
+          }
+
+          .start-btn {
+            display: inline-block;
+            padding: 10px;
+            border: 1px solid green;
+            border-radius: 4px;
+            text-decoration: none;
+            color: green;
+            font-size: 16px;
+            cursor: pointer;
+            background: transparent;
+          }
+
+          .join-title {
+            text-align: center;
+            font-weight: bold;
+            margin-bottom: 20px;
+          }
+
+          .room-input {
+            padding: 10px;
+            font-size: 16px;
+            width: 80px;
+            text-align: center;
+            margin-right: 10px;
+            border: 1px solid #eee;
           }
         `}</style>
       </div>
