b8c03ed2f2e6eb093e07cd51b077e91ead637b1f
Aurelio Pita <jaapaurelio@gmail.com>
2019-01-27 04:18:14 +0000

    Improve i18n

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ class MyApp extends App {
       pageProps = await Component.getInitialProps(ctx);
     }
 
-    return { pageProps };
+    return { pageProps, namespacesRequired: ["common"] };
   }
 
   render() {
