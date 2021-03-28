308f1a0b52da6aea0d1aef74a7860b4280aa14a0
Aurelio Pita <aurelio.pita@blip.pt>
2021-03-28 21:16:47 +0100

    next offline

--- next.config.js
+++ next.config.js
@@ -3,31 +3,46 @@ const withOffline = require('next-offline')
 require('dotenv').config()
 const withTM = require('next-transpile-modules')
 
-module.exports = withOffline(
-    withTM({
-        webpack: (config, { isServer, buildId, dev }) => {
-            const env = Object.keys(process.env).reduce((acc, curr) => {
-                acc[`process.env.${curr}`] = JSON.stringify(process.env[curr])
-                return acc
-            }, {})
-
-            config.plugins.push(new webpack.DefinePlugin(env))
-
-            return config
-        },
-        generateInDevMode: true,
+module.exports = {
+    ...withOffline({
         workboxOpts: {
             swDest: process.env.NEXT_EXPORT
                 ? 'service-worker.js'
                 : 'static/service-worker.js',
             runtimeCaching: [
                 {
-                    urlPattern: /^https?.*/,
-                    handler: 'NetworkFirst',
+                    urlPattern: '/',
+                    handler: 'networkFirst',
                     options: {
-                        cacheName: 'offlineCache',
-                        expiration: {
-                            maxEntries: 200,
+                        cacheName: 'html-cache',
+                    },
+                },
+                {
+                    urlPattern: /api/,
+                    handler: 'networkOnly',
+                    options: {
+                        cacheName: 'internal-api',
+                    },
+                },
+                {
+                    urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
+                    handler: 'cacheFirst',
+                    options: {
+                        cacheName: 'image-cache',
+                        cacheableResponse: {
+                            statuses: [0, 200],
+                        },
+                    },
+                },
+                {
+                    urlPattern: new RegExp(
+                        '^https://api.themoviedb.org/3/movie'
+                    ),
+                    handler: 'staleWhileRevalidate',
+                    options: {
+                        cacheName: 'api-cache',
+                        cacheableResponse: {
+                            statuses: [200],
                         },
                     },
                 },
@@ -41,6 +56,19 @@ module.exports = withOffline(
                 },
             ]
         },
+    }),
+    ...withTM({
+        webpack: (config, { isServer, buildId, dev }) => {
+            const env = Object.keys(process.env).reduce((acc, curr) => {
+                acc[`process.env.${curr}`] = JSON.stringify(process.env[curr])
+                return acc
+            }, {})
+
+            config.plugins.push(new webpack.DefinePlugin(env))
+
+            return config
+        },
+        generateInDevMode: true,
         transpileModules: ['moviedb-promise'],
-    })
-)
+    }),
+}
